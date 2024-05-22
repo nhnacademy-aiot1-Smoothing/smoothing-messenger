ecf15cc9e23622125371f18c39fe7cb5f7af8467
Jiyoon <bystar1687@nate.com>
2024-05-22 17:53:54 +0900

    [Refactor]: 안 쓰는 클래스, 메서드 삭제

--- src/main/java/live/smoothing/messenger/hook/DoorayHook.java
+++ src/main/java/live/smoothing/messenger/hook/DoorayHook.java
@@ -1,28 +1,20 @@
 package live.smoothing.messenger.hook;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 /**
  * Dooray hook 요소 설정 클래스
  *
  * @author 김지윤
  */
-@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class DoorayHook {
 
     private String botName;
 
-    private String botIconImage;
-
     private String text;
-
-    private List<Attachment> attachments;
 
     public static DoorayHookBuilder builder() {
 
@@ -33,21 +25,12 @@ public class DoorayHook {
 
         private String botName;
 
-        private String botIconImage;
-
         private String text;
 
-        private List<Attachment> attachments;
 
         public DoorayHookBuilder botName(String botName) {
 
             this.botName = botName;
-            return this;
-        }
-
-        public DoorayHookBuilder botIconImage(String botIconImage) {
-
-            this.botIconImage = botIconImage;
             return this;
         }
 
@@ -57,88 +40,9 @@ public class DoorayHook {
             return this;
         }
 
-        public DoorayHookBuilder attachments(List<Attachment> attachments) {
-
-            this.attachments = attachments;
-            return this;
-        }
-
         public DoorayHook build() {
 
-            return new DoorayHook(this.botName, this.botIconImage, this.text, this.attachments);
-        }
-
-        public String toString() {
-
-            return "DoorayHook.DoorayHookBuilder(botName=" + this.botName + ", botIconImage=" + this.botIconImage + "text=" + this.text + ", attachments=" + this.attachments + ")";
+            return new DoorayHook(this.botName, this.text);
         }
     }
-
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class Attachment {
-
-        private String title;
-
-        private String titleLink;
-
-        private String text;
-
-        private String color;
-
-        public static AttachmentBuilder builder() {
-
-            return new AttachmentBuilder();
-        }
-
-        @NoArgsConstructor
-        public static class AttachmentBuilder {
-
-            private String title;
-
-            private String titleLink;
-
-            private String text;
-
-            private String color;
-
-            public AttachmentBuilder title(String title) {
-
-                this.title = title;
-                return this;
-            }
-
-            public AttachmentBuilder titleLink(String titleLink) {
-
-                this.titleLink = titleLink;
-                return this;
-            }
-
-            public AttachmentBuilder text(String text) {
-
-                this.text = text;
-                return this;
-            }
-
-            public AttachmentBuilder color(String color) {
-
-                this.color = color;
-                return this;
-            }
-
-            public Attachment build() {
-
-                return new Attachment(this.title, this.titleLink, this.text, this.color);
-            }
-
-            public String toString() {
-
-                return "DoorayHook.Attachment.AttachmentBuilder(title=" + this.title + ", titleLink=" + this.titleLink + ", text=" + this.text + ", color=" + this.color + ")";
-            }
-        }
-
-
-    }
-
 }
